3cbb24d908c6f617c6837459fd06b892e44e9280
Karen Nuñez-Reza <kajunure@gmail.com>
2022-04-25 17:45:33 -0500

    class five

---
+++ WWC-Fullstack-main/react-fe/src/components/pages/HomePage.js
@@ -0,0 +1,11 @@
+import React from "react";
+import FeaturedPost from "../FeaturedPost";
+import ListPost from "../ListPost";
+
+const HomePage = ({posts, onEdit}) => {
+  return(
+      <FeaturedPost
+  )
+};
+
+export default HomePage;
